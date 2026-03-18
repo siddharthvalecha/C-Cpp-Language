b425f6d9e3b9a891e3e651cfa32c7c088dbec0a4
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-03-18 23:20:01 +0530

    Added a prog for for loop

---
+++ C Language/forloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=0;i<10;i++)
+    {
+        printf("Value of i is %d\n",i);
+    }
+
+    for(int i=10;i>0;i--)
+    {
+        printf("Value of i is %d\n",i);
+    }
+
+}
