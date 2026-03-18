d3e641b0aa9ff6902fb59de0c5086aced7d3306c
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-03-18 23:17:13 +0530

    Added a prog printing numb. using while loop

---
+++ C Language/while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=1;
+
+    while (a<=10)
+    {
+        printf("1=> value of a=%d\n",a);
+        a++;
+    }
+
+    //a=1;
+    while (a<=20)
+    {
+        printf("2=> value of a=%d\n",a);
+        a++;
+    }
+    
+    
+}
